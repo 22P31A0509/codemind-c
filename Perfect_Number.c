84cf826bbb3b53d22d7bcc3659118e691ffdaf99
22P31A0509 <119730222+22P31A0509@users.noreply.github.com>
2022-12-15 13:01:22 +0530

    #include<stdio.h> int main() {     int num,n,sum=0,i;     scanf("%d",&num);     for(i=1;i<num;i++)     {         if(num%i==0)         {             sum += i;         }     }         if(sum == num)         {             printf("True");         }         else         {             printf("False");         }
    }

---
+++ Perfect_Number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int num,n,sum=0,i;
+    scanf("%d",&num);
+    for(i=1;i<num;i++)
+    {
+        if(num%i==0)
+        {
+            sum += i;
+        }
+    }
+        if(sum == num)
+        {
+            printf("True");
+        }
+        else
+        {
+            printf("False");
+        }
+    
+}
